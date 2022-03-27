e374e8d4e165bc7e3fad4e731e12c09e60b5a11a
Azurene <98633770+Azurene@users.noreply.github.com>
2022-03-26 22:33:56 -0700

    confirmation text and object things

--- Develop/script.js
+++ Develop/script.js
@@ -1,14 +1,12 @@
 // Assignment code here
-passwordOptions = {
-  passwordLength: 0,
-  passwordLowercase: false,
-  passwordUppercase: false,
-  passwordNumeric: false,
-  passwordSpecial: false
+var createdPassword = {
+  length: 0,
+  lowercase: false,
+  uppercase: false,
+  numeric: false,
+  special: false,
 }
 
-
-// test
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
@@ -17,39 +15,40 @@ function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
-// ask for password criteria
-// how many characters should the password be?
-var changeLength = window.prompt("How many characters would you like your password to be?");
-passwordOptions.passwordLength = changeLength;
-  
-// what character types should be included?
-  var generatePassword = function() {
-    // include lowercase?
-    var confirmLowercase = window.confirm("Would you like your password to have lowercase letters?");
-    if (confirmLowercase) {
-      passwordCharacteristics.passwordLowercase = true;
-    }
-    // include uppercase?
-    var confirmUppercase = window.confirm("Would you like your password to have uppercase letters?");
-    if (confirmUppercase) {
-      passwordCharacteristics.passwordUppercase = true;
-    }
-    // include numeric characters?
-    var confirmNumeric = window.confirm("Would you like your password to have numeric characters?");
-    if (confirmNumeric) {
-      passwordCharacteristics.passwordNumeric = true;
-    }
-    // add special characters?
-    var confirmSpecial = window.confirm("Would you like your password to have special characters?");
-    if (confirmSpecial) {
-      passwordCharacteristics.passwordSpecial = true;
-    }
-    passwordText.value = password;
-  }
-  console.log(passwordOptions)
-};
+  passwordText.value = password;
+}
 
-writePassword();
+var generatePassword = function() {
+  // ask for length of password
+  var passwordLength = window.prompt("How many characters would you like your password to be?");
+  passwordLength = createdPassword.length
+  // choose length between 8 and 128 characters
+  if (createdPassword.length > 8  || createdPassword.length < 128) {
+    
+  }
+  // ask for character types
+  // ask lowercase
+  var confirmLowercase = window.confirm("Would you like your password to have lowercase letters?");
+  if (confirmLowercase === true) {
+    createdPassword.lowercase = true;
+  }
+  // ask uppercase
+  var confirmUppercase = window.confirm("Would you like your password to have uppercase letters?");
+  if (confirmUppercase === true) {
+    createdPassword.uppercase = true;
+  }
+  // ask numeric
+  var confirmNumeric = window.confirm("Would you like your password to have numbers?");
+  if (confirmNumeric === true) {
+    createdPassword.numeric = true;
+  }
+  // ask special characters
+  var confirmSpecial = window.confirm("Would you like your password to have special characters?");
+  if (confirmSpecial === true) {
+    createdPassword.special = true;
+  }
+  console.log(createdPassword);
+}
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
